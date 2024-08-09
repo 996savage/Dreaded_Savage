5fce12e0a1e29779a02df53bb5634c6d190a2bb7
996savage <167668791+996savage@users.noreply.github.com>
2024-08-09 22:15:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,18 +6,18 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "davidcyril209@gmail.com";
-global.location = "Abuja, Nigeria";
+global.email = "lordofbots7@gmail.com";
+global.location = "Nairobi, Kenya";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
-global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
-global.github = process.env.GITHUB || "https://github.com/DeeCeeXxx/Queen_Anita-V2";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
-global.devs = "https://t.me/deecee_x";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Nairobi";
+global.github = process.env.GITHUB || "https://github.com/996savage/Dreaded_Savage";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VagAm55K5cDIUJ7JZ71N";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029VagAm55K5cDIUJ7JZ71N";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/880fbc3c4010707795329.jpg";
+global.devs = "https://t.me/Liljaysavage";
 global.sudo = process.env.SUDO || "";
 global.owner = process.env.OWNER_NUMBER || "";
 global.style = process.env.STYLE || "3";
@@ -27,8 +27,7 @@ global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
-global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/a22200a780671e0e32383.jpg,https://telegra.ph/file/ba9ced500f9eca7db8acb.mp4";
-global.waPresence = process.env.WAPRESENCE || "available";
+global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/880fbc3c4010707795329.jpg,https://telegra.ph/file/a3ae519fcf5c093950587.mp4"
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
@@ -48,11 +47,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`QUEEN_ANITA-V2™`",
-  author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
-  packname: process.env.PACK_NAME || "A N I T A",
-  botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  caption: process.env.CAPTION || "`Dreaded_Savage™`",
+  author: process.env.PACK_AUTHER || "DREADED_SAVAGE-",
+  packname: process.env.PACK_NAME || "D R E A D E D",
+  botname: process.env.BOT_NAME || "DREADED_SAVAGE",
+  ownername: process.env.OWNER_NAME || "Liljay savage",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -64,7 +63,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
-  LANG: (process.env.THEME || "A N I T A").toUpperCase(),
+  LANG: (process.env.THEME || "D R E A D E D").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
